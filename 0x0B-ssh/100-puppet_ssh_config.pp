f0e59e0dc695efac149772700e3396cb3274b733
Manuel254 <immanuelkituku@gmail.com>
2023-03-26 06:37:41 -0700

    Create ssh config file with options to turn off password authentication and declare identity file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-# Turn off password authentication
-exec { 'Turn off passwd auth':
-  command => '/bin/sed -i "s/^#?\\s*PasswordAuthentication\\s.*/PasswordAuthentication no/g" /etc/ssh/sshd_config',
-  unless  => '/bin/grep -q "^\\s*PasswordAuthentication\\s*no\\s*$" /etc/ssh/sshd_config',
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-# Declare identity file
-exec { 'Declare identity file':
-  command => '/bin/echo -e "\nIdentityFile /path/to/identity/file" >> /etc/ssh/ssh_config',
-  unless  => '/bin/grep -q "^\\s*IdentityFile\\s*/path/to/identity/file\\s*$" /etc/ssh/ssh_config',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
